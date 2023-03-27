2901f3837687a1fcccb756c18a9119aa1be85b59
talattias11 <123408037+talattias11@users.noreply.github.com>
2023-03-27 06:11:28 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+//1 Area of the Rectangle
+// let height = prompt("Please Enter the Height of the rectangle")
+// let width = prompt("Please Enter the Width of the rectangle")
+// if (height === width) {
+//     console.log("The rectangle is a Square")
+// } else
+//     console.log("The Area of the Rectangle is: " + height * width)
+
+//2 Age by Year Provided
+// let birthYear = prompt("Please Enter Your Birth Year")
+// let age = 2022 - birthYear
+// console.log("Your Age is: " + age)
+
+//3 לולאה שעוברת על המערך ומדפיסה את הפריטים במערך בסדר הפוך.
+// let array1 = []
+// for (let i = 0; i < 6 ; i++) {
+//     var product = prompt("Please Enter Products")
+//     array1.push(product)
+// }
+// console.log(array1)
+// let array2 = []
+// for (let j = 0; j < 6; j++) {
+//     array2.push(array1.pop())
+// }
+// console.log(array2)
+
+//4 WorkDay or WeekEnd
+// let weekDay = prompt("Please Enter a Number from 1-7 to Present a Week Day")
+// if ((weekDay < 1) || (weekDay > 7)) {
+//     console.log("Please Enter a Valid Week Day")
+// } else if ((weekDay >= 1) && (weekDay <= 5)) {
+//         console.log("It's A WorkDay")
+//         } else 
+//             console.log("It's The WeekEnd 🥳")
+
+//5 names on the unEven spots of the array
+// let namesArray = []
+// let unEvenspotsNames = []
+// for (let index = 0; index < 6 ; index++) {
+//     let nameItems = prompt("Please Enter Names")
+//     namesArray.push(nameItems)
+//     if (index % 2 != 0) {
+//         unEvenspotsNames.push(nameItems)
+//     }
+// }
+// console.log(unEvenspotsNames)
